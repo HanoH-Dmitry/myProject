194e5b1fcbc707b0786e7237a28f4aea7845757c
HanoH-Dmitry <37418341+HanoH-Dmitry@users.noreply.github.com>
2020-12-03 14:32:01 +0300

    fgfd

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 async function getResponse() {
 	let response = await fetch('https://jsonplaceholder.typicode.com/photos')
-	let content = response.text()
+	let content = await response.text()
 	console.log(content)
 }
 getResponse()
