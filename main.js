03ea39cd68d05a3f2e70754fd347707d90a0ef88
HanoH-Dmitry <37418341+HanoH-Dmitry@users.noreply.github.com>
2020-12-03 14:33:46 +0300

    vfDAS

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 async function getResponse() {
 	let response = await fetch('https://jsonplaceholder.typicode.com/photos')
 	let content = await response.text()
-	console.log(content)
+	console.log(JSON.parse(content))
 }
 getResponse()
